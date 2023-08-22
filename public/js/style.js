abb2ac49cf6468e69b1a31613ab2529a502d2c47
Aiyub150 <115766780+Aiyub150@users.noreply.github.com>
2023-08-22 08:29:24 +0700

    Update style.js

--- public/js/style.js
+++ public/js/style.js
@@ -9,9 +9,23 @@ document.getElementById("showPopupBtn").addEventListener("click", function() {
   });
 
 window.onscroll = () => {
-let header = document.querySelector('.header');
-
-header.classList.toggle('sticky', window.scrollY > 100);
+    sections.forEach(sec => {
+          let top = window.scrollY;
+          let offset = sec.offsetTop - 150;
+          let height = sec.offsetHeight; // Menggunakan offsetHeight bukan offsetheight
+          let id = sec.getAttribute('id');
+  
+          if (top >= offset && top < offset + height) {
+              navLinks.forEach(link => {
+                  link.classList.remove('active');
+              });
+              document.querySelector(`header nav a[href*='${id}']`).classList.add('active'); // Perbaikan tanda petik
+          }
+      });
+  
+      let header = document.querySelector('.header');
+  
+      header.classList.toggle('sticky', window.scrollY > 100);
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
